5973894d75b168f9dc85ccb71b245f582ebccd98
MR.Z <42929278+MrzHack@users.noreply.github.com>
2018-09-14 03:06:03 +0700

    Update instahack.py

--- instahack.py
+++ instahack.py
@@ -6,7 +6,7 @@ TODO LIST:
 	Add help function to all "Yes/no" questions
 	Add help  function to "Press enter to exit input"
 '''
-import requests
+import instahack
 import json
 import time
 import os
@@ -139,7 +139,7 @@ class Instabrute():
 
 
 
-instabrute = Instabrute(Input(' Masukkan IG Target: '))
+instabrute = Instabrute(Input(' Masukan ID/EMAIL/NAMA IG Target: '))
 
 try:
 	delayLoop = int(Input('[*] Masukkan Limit/Jeda (in seconds): ')) 
